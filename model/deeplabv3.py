529bca3b94ddf89c0018bb6da373240c46466a40
ludas <luchao256258@126.com>
2019-04-16 15:38:45 +0800

    Update deeplabv3.py
    change for run

--- model/deeplabv3.py
+++ model/deeplabv3.py
@@ -13,7 +13,7 @@ class DeepLabV3(nn.Module):
     def __init__(self, model_id, project_dir):
         super(DeepLabV3, self).__init__()
 
-        self.num_classes = 20
+        self.num_classes = 20################
 
         self.model_id = model_id
         self.project_dir = project_dir
@@ -39,7 +39,8 @@ class DeepLabV3(nn.Module):
     def create_model_dirs(self):
         self.logs_dir = self.project_dir + "/training_logs"
         self.model_dir = self.logs_dir + "/model_%s" % self.model_id
-        self.checkpoints_dir = self.model_dir + "/checkpoints"
+        #self.checkpoints_dir = self.model_dir + "/checkpoints"
+        self.checkpoints_dir = self.model_dir 
         if not os.path.exists(self.logs_dir):
             os.makedirs(self.logs_dir)
         if not os.path.exists(self.model_dir):
