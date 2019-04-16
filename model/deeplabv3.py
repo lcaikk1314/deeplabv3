269f3ac4e83b3c7d084f4e62e2c4c82236303626
ludas <luchao256258@126.com>
2019-04-16 15:39:10 +0800

    Update deeplabv3.py
    change for run 2

--- model/deeplabv3.py
+++ model/deeplabv3.py
@@ -1,48 +1,233 @@
 # camera-ready
 
+# NOTE! OS: output stride, the ratio of input image resolution to final output resolution (OS16: output size is (img_h/16, img_w/16)) (OS8: output size is (img_h/8, img_w/8))
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
 
-import os
+def make_layer(block, in_channels, channels, num_blocks, stride=1, dilation=1):
+    strides = [stride] + [1]*(num_blocks - 1) # (stride == 2, num_blocks == 4 --> strides == [2, 1, 1, 1])
 
-from resnet import ResNet18_OS16, ResNet34_OS16, ResNet50_OS16, ResNet101_OS16, ResNet152_OS16, ResNet18_OS8, ResNet34_OS8
-from aspp import ASPP, ASPP_Bottleneck
+    blocks = []
+    for stride in strides:
+        blocks.append(block(in_channels=in_channels, channels=channels, stride=stride, dilation=dilation))
+        in_channels = block.expansion*channels
 
-class DeepLabV3(nn.Module):
-    def __init__(self, model_id, project_dir):
-        super(DeepLabV3, self).__init__()
+    layer = nn.Sequential(*blocks) # (*blocks: call with unpacked list entires as arguments)
 
-        self.num_classes = 20################
+    return layer
 
-        self.model_id = model_id
-        self.project_dir = project_dir
-        self.create_model_dirs()
+class BasicBlock(nn.Module):
+    expansion = 1
 
-        self.resnet = ResNet18_OS8() # NOTE! specify the type of ResNet here
-        self.aspp = ASPP(num_classes=self.num_classes) # NOTE! if you use ResNet50-152, set self.aspp = ASPP_Bottleneck(num_classes=self.num_classes) instead
+    def __init__(self, in_channels, channels, stride=1, dilation=1):
+        super(BasicBlock, self).__init__()
+
+        out_channels = self.expansion*channels
+
+        self.conv1 = nn.Conv2d(in_channels, channels, kernel_size=3, stride=stride, padding=dilation, dilation=dilation, bias=False)
+        self.bn1 = nn.BatchNorm2d(channels)
+
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=dilation, dilation=dilation, bias=False)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+        if (stride != 1) or (in_channels != out_channels):
+            conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+            bn = nn.BatchNorm2d(out_channels)
+            self.downsample = nn.Sequential(conv, bn)
+        else:
+            self.downsample = nn.Sequential()
+
+    def forward(self, x):
+        # (x has shape: (batch_size, in_channels, h, w))
+
+        out = F.relu(self.bn1(self.conv1(x))) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+        out = self.bn2(self.conv2(out)) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+
+        out = out + self.downsample(x) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+
+        out = F.relu(out) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+
+        return out
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels, channels, stride=1, dilation=1):
+        super(Bottleneck, self).__init__()
+
+        out_channels = self.expansion*channels
+
+        self.conv1 = nn.Conv2d(in_channels, channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(channels)
+
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=stride, padding=dilation, dilation=dilation, bias=False)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+        self.conv3 = nn.Conv2d(channels, out_channels, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        if (stride != 1) or (in_channels != out_channels):
+            conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+            bn = nn.BatchNorm2d(out_channels)
+            self.downsample = nn.Sequential(conv, bn)
+        else:
+            self.downsample = nn.Sequential()
+
+    def forward(self, x):
+        # (x has shape: (batch_size, in_channels, h, w))
+
+        out = F.relu(self.bn1(self.conv1(x))) # (shape: (batch_size, channels, h, w))
+        out = F.relu(self.bn2(self.conv2(out))) # (shape: (batch_size, channels, h, w) if stride == 1, (batch_size, channels, h/2, w/2) if stride == 2)
+        out = self.bn3(self.conv3(out)) # (shape: (batch_size, out_channels, h, w) if stride == 1, (batch_size, out_channels, h/2, w/2) if stride == 2)
+
+        out = out + self.downsample(x) # (shape: (batch_size, out_channels, h, w) if stride == 1, (batch_size, out_channels, h/2, w/2) if stride == 2)
+
+        out = F.relu(out) # (shape: (batch_size, out_channels, h, w) if stride == 1, (batch_size, out_channels, h/2, w/2) if stride == 2)
+
+        return out
+
+class ResNet_Bottleneck_OS16(nn.Module):
+    def __init__(self, num_layers):
+        super(ResNet_Bottleneck_OS16, self).__init__()
+
+        if num_layers == 50:
+            resnet = models.resnet50()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/root/deeplabv3/pretrained_models/resnet/resnet50-19c8e357.pth"))
+            # remove fully connected layer, avg pool and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-3])
+
+            print ("pretrained resnet, 50")
+        elif num_layers == 101:
+            resnet = models.resnet101()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/workspace/mnt/group/other/luchao/deeplabv3/deeplabv3/pretrained_models/resnet/resnet101-5d3b4d8f.pth"))
+            # remove fully connected layer, avg pool and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-3])
+
+            print ("pretrained resnet, 101")
+        elif num_layers == 152:
+            resnet = models.resnet152()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/workspace/mnt/group/other/luchao/deeplabv3/deeplabv3/pretrained_models/resnet/resnet152-b121ed2d.pth"))
+            # remove fully connected layer, avg pool and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-3])
+
+            print ("pretrained resnet, 152")
+        else:
+            raise Exception("num_layers must be in {50, 101, 152}!")
+
+        self.layer5 = make_layer(Bottleneck, in_channels=4*256, channels=512, num_blocks=3, stride=1, dilation=2)
 
     def forward(self, x):
         # (x has shape (batch_size, 3, h, w))
 
-        h = x.size()[2]
-        w = x.size()[3]
+        # pass x through (parts of) the pretrained ResNet:
+        c4 = self.resnet(x) # (shape: (batch_size, 4*256, h/16, w/16)) (it's called c4 since 16 == 2^4)
 
-        feature_map = self.resnet(x) # (shape: (batch_size, 512, h/16, w/16)) (assuming self.resnet is ResNet18_OS16 or ResNet34_OS16. If self.resnet is ResNet18_OS8 or ResNet34_OS8, it will be (batch_size, 512, h/8, w/8). If self.resnet is ResNet50-152, it will be (batch_size, 4*512, h/16, w/16))
-
-        output = self.aspp(feature_map) # (shape: (batch_size, num_classes, h/16, w/16))
-
-        output = F.upsample(output, size=(h, w), mode="bilinear") # (shape: (batch_size, num_classes, h, w))
+        output = self.layer5(c4) # (shape: (batch_size, 4*512, h/16, w/16))
 
         return output
 
-    def create_model_dirs(self):
-        self.logs_dir = self.project_dir + "/training_logs"
-        self.model_dir = self.logs_dir + "/model_%s" % self.model_id
-        #self.checkpoints_dir = self.model_dir + "/checkpoints"
-        self.checkpoints_dir = self.model_dir 
-        if not os.path.exists(self.logs_dir):
-            os.makedirs(self.logs_dir)
-        if not os.path.exists(self.model_dir):
-            os.makedirs(self.model_dir)
-            os.makedirs(self.checkpoints_dir)
+class ResNet_BasicBlock_OS16(nn.Module):
+    def __init__(self, num_layers):
+        super(ResNet_BasicBlock_OS16, self).__init__()
+
+        if num_layers == 18:
+            resnet = models.resnet18()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/workspace/mnt/group/other/luchao/deeplabv3/deeplabv3/pretrained_models/resnet/resnet18-5c106cde.pth"))
+            # remove fully connected layer, avg pool and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-3])
+
+            num_blocks = 2
+            print ("pretrained resnet, 18")
+        elif num_layers == 34:
+            resnet = models.resnet34()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/workspace/mnt/group/other/luchao/deeplabv3/deeplabv3/pretrained_models/resnet/resnet34-333f7ec4.pth"))
+            # remove fully connected layer, avg pool and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-3])
+
+            num_blocks = 3
+            print ("pretrained resnet, 34")
+        else:
+            raise Exception("num_layers must be in {18, 34}!")
+
+        self.layer5 = make_layer(BasicBlock, in_channels=256, channels=512, num_blocks=num_blocks, stride=1, dilation=2)
+
+    def forward(self, x):
+        # (x has shape (batch_size, 3, h, w))
+
+        # pass x through (parts of) the pretrained ResNet:
+        c4 = self.resnet(x) # (shape: (batch_size, 256, h/16, w/16)) (it's called c4 since 16 == 2^4)
+
+        output = self.layer5(c4) # (shape: (batch_size, 512, h/16, w/16))
+
+        return output
+
+class ResNet_BasicBlock_OS8(nn.Module):
+    def __init__(self, num_layers):
+        super(ResNet_BasicBlock_OS8, self).__init__()
+
+        if num_layers == 18:
+            resnet = models.resnet18()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/workspace/mnt/group/other/luchao/deeplabv3/deeplabv3/pretrained_models/resnet/resnet18-5c106cde.pth"))
+            # remove fully connected layer, avg pool, layer4 and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-4])
+
+            num_blocks_layer_4 = 2
+            num_blocks_layer_5 = 2
+            print ("pretrained resnet, 18")
+        elif num_layers == 34:
+            resnet = models.resnet34()
+            # load pretrained model:
+            resnet.load_state_dict(torch.load("/workspace/mnt/group/other/luchao/deeplabv3/deeplabv3/pretrained_models/resnet/resnet34-333f7ec4.pth"))
+            # remove fully connected layer, avg pool, layer4 and layer5:
+            self.resnet = nn.Sequential(*list(resnet.children())[:-4])
+
+            num_blocks_layer_4 = 6
+            num_blocks_layer_5 = 3
+            print ("pretrained resnet, 34")
+        else:
+            raise Exception("num_layers must be in {18, 34}!")
+
+        self.layer4 = make_layer(BasicBlock, in_channels=128, channels=256, num_blocks=num_blocks_layer_4, stride=1, dilation=2)
+
+        self.layer5 = make_layer(BasicBlock, in_channels=256, channels=512, num_blocks=num_blocks_layer_5, stride=1, dilation=4)
+
+    def forward(self, x):
+        # (x has shape (batch_size, 3, h, w))
+
+        # pass x through (parts of) the pretrained ResNet:
+        c3 = self.resnet(x) # (shape: (batch_size, 128, h/8, w/8)) (it's called c3 since 8 == 2^3)
+
+        output = self.layer4(c3) # (shape: (batch_size, 256, h/8, w/8))
+        output = self.layer5(output) # (shape: (batch_size, 512, h/8, w/8))
+
+        return output
+
+def ResNet18_OS16():
+    return ResNet_BasicBlock_OS16(num_layers=18)
+
+def ResNet34_OS16():
+    return ResNet_BasicBlock_OS16(num_layers=34)
+
+def ResNet50_OS16():
+    return ResNet_Bottleneck_OS16(num_layers=50)
+
+def ResNet101_OS16():
+    return ResNet_Bottleneck_OS16(num_layers=101)
+
+def ResNet152_OS16():
+    return ResNet_Bottleneck_OS16(num_layers=152)
+
+def ResNet18_OS8():
+    return ResNet_BasicBlock_OS8(num_layers=18)
+
+def ResNet34_OS8():
+    return ResNet_BasicBlock_OS8(num_layers=34)
